e83a2a9f70be68c5882f9220f3afa8b0d11dc667
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-05-02 19:47:05 +0100

    Create stats.cpp

---
+++ stats.cpp
@@ -0,0 +1,48 @@
+Total Kata 5,473,207
+POS #504,692
+
+Js
+    5,611 Kata
+
+
+Python
+     4,570 Kata
+
+
+C#
+    2,118 Kata
+
+
+Haskel
+    1,809 Kata
+
+
+Java
+     1,701 Kata
+
+
+C
+    1,339 Kata
+
+
+
+CoffeScript
+      1,256 Kata
+
+
+C++
+    1,126 Kata
+
+
+Rust
+    907 Kata
+
+
+PHP
+    897 Kata
+   
+ 
+ Go
+      676 Kata
+      
+ 
