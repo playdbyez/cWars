b627276d5ec589dbefd795b5f2d4e03aa8c362ad
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-05-05 16:23:02 +0100

    Update tr4vell1ng.cpp

--- tr4vell1ng.cpp
+++ tr4vell1ng.cpp
@@ -97,3 +97,64 @@ cout << "\n";
 for(int h = 0; h<tarq.size(); h++){ cout << tarq[h] << " ";}
 cout << "\n";
 }
+
+
+
+
+
+
+
+
+//Best Optimization so far
+
+
+#include <iostream> 
+#include <string> 
+#include <vector> 
+#include <algorithm> 
+#include <chrono> 
+using namespace std; 
+ int main(){
+                                        auto start = std::chrono::high_resolution_clock::now();
+vector<int> ls {50, 55, 56, 57, 58};     
+vector<int> to;
+vector<int> d;
+vector<int>sums;
+vector<int>tart;
+
+int k = 3;
+int t = 163;
+unsigned long mn = k+1;
+int x = 0;
+int anz =-1;
+int si = ls.size();
+
+
+sort( ls.begin(), ls.end());
+do{ for(size_t i = 0; i<ls.size(); i++){to.push_back(ls[i]);} 
+    to.push_back(9999); } 
+        while(next_permutation(ls.begin(),ls.end()));
+
+
+for(size_t j = 0; j<to.size(); j++){
+    for(size_t ku = 1; ku < mn; ku++)
+        {if (to[j] == 9999)d.push_back(to[j+ku]);}{d.push_back(9999);}}
+
+for (int c =0; c< d.size(); c++)
+{if (d[c] != 9999){x+=d[c];}
+if (d[c] == 9999 && x !=0){ if (x<=t){sums.push_back(x);} x=0;}
+}
+sort(sums.begin(), sums.end(),  greater());
+anz = sums[0];
+   if (k > si){anz = -1;}
+if (anz == 0){anz = -1;}
+
+cout << anz;
+auto stop = std::chrono::high_resolution_clock::now();
+auto duration = duration_cast<std::chrono::microseconds>(stop - start);
+
+cout << "\n" << duration.count() << " microseconds" ;
+}
+
+
+
