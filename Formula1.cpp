15649e502616cbf9e45db38117a4466043b99e7e
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-05-12 21:22:57 +0100

    Create Formula1.cpp

---
+++ Formula1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<map>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+
+
+int main ()
+{
+    string events="2 O 17 I";
+
+int champion =17;
+int posit = champion;
+ 
+  vector <char> sig;
+   vector <int> sim;
+ int f;
+ 
+  for (auto i: events)
+  {if (isalpha(i)){ sig.push_back(i);}}
+ 
+ 
+for (size_t l=0; l< events.size(); l++)
+  {
+        if (!isdigit(events[l]) && isdigit(events[l-1]) && !isdigit(events[l-2])){sim.push_back(events[l-1]-48);}
+       
+    }
+ 
+  for (size_t y =0; y<sim.size(); y++){cout << sim[y]<< " ";}
+
+      }
