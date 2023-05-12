2d233646bf18a99d6f561202e607a4266f267169
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-05-12 21:24:29 +0100

    Update ExecutionTIMER.cpp

--- ExecutionTIMER.cpp
+++ ExecutionTIMER.cpp
@@ -1,8 +1,44 @@
+#include <vector> 
+#include <algorithm> 
+
 #include<chrono>
+using namespace std;
+  
+  
 auto start = std::chrono::high_resolution_clock::now();
 
 
-//Code here
+
+class BestTravel
+{
+public:
+    static int chooseBestSum(int t, int k, std::vector<int>& ls);
+};
+
+int BestTravel::chooseBestSum(int t, int k, std::vector<int>& ls){
+vector<int> to;
+
+
+int x = 0;
+int anz =-1;
+size_t si = ls.size();
+ 
+ if (static_cast<size_t>(k) > si){return -1;}
+          sort( ls.begin(), ls.end(), greater());
+                
+do{ for(size_t i = 0; i< static_cast<size_t>(k); i++)
+{
+x+=ls[i];} 
+if(x==t){ return x;}
+if(x<=t){ if(std::find(to.begin(), to.end(), x) == to.end()){to.push_back(x);} } 
+x=0;
+}
+while(prev_permutation(ls.begin(),ls.end()));
+
+ anz = *max_element(to.begin(), to.end());
+if (to.empty()){return -1;}        
+cout << anz;
+}
 
 
 auto stop = std::chrono::high_resolution_clock::now();
