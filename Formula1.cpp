2c641e7d2ef32455d9ddcd1675e8209cc59ec083
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-06-02 10:59:15 +0100

    Update Formula1.cpp

--- Formula1.cpp
+++ Formula1.cpp
@@ -1,39 +1,17 @@
-#include<iostream>
-#include<map>
-#include<string>
-#include<vector>
-
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-
-
-int main ()
+int main()
 {
-    string events="9 O 17 O 9 O 12 O 2 O 12 O 9 O 1 O 5 O 12 O 17 O 20 O 16 O 7 O 2 O 8 O 16 O 14 O 3 O 14 O 11 O 16 O 1 O 13 O 8 O 14 O 5 O 12 O 4 O";
+  string events = "9 O 17 O 9 O 12 O 2 O 12 O 9 O 1 O 5 O 12 O 17 O 20 O 16 O 7 O 2 O 8 O 16 O 14 O 3 O 14 O 3 I 11 O 16 O 1 O 13 O 8 O 14 O 5 O 12 O 4 O";
 
-int champion =12;
-int rac1 =1;
-int rac2 =2;
-int rac3 =3;
-int rac4 =4;
-int rac5 =5;
-int rac6 =6;
-int rac7 =7;
-int rac9 =9;
-int rac10 =10;
-int rac11 =11;
-int rac12 =12;
-int rac13 =13;
-int rac14 =14;
-int rac15 =15;
-int rac16 =16;
-int rac17 =17;
-int rac18 =18;
-int rac19 =19;
-int rac20 =20;
-
-int posit = champion;
-   vector <int> sim;
+  vector <int> race = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+int hold =0;
+    int champ = 9;
+        vector <int> sim;
  
  
  
@@ -44,20 +22,40 @@ for (size_t l=0; l< events.size(); l++)
        if (events[l] == 'O'){ sim.push_back(0);}
        if (events[l] == 'I'){ sim.push_back(99);}
     }
- 
- 
- for (size_t i=0; i< sim.size(); i++)
-  { if (posit > 1){
-      if (sim[i] == champion && sim[i+1] == 0 ){posit--;}
-      if (sim[i] == champion && sim[i+1] == 99 ){posit = -1; return posit;}
-      if (sim[i] != champion && sim[i] == 99){posit--;}
-  }
-      }
- 
- 
- 
- 
- 
-cout << posit;
+
+
+for (size_t g =0; g<sim.size(); g++)
+{
+if (sim[g] == 99 && sim[g-1] == champ){cout << "Champion CRASHED!";return -1;}									
+
+if (sim[g] == 0)
+		{for (size_t l =0; l<race.size(); l++)
+				{if (race[l] == sim[g-1]){hold = race[l]; race[l] = race[l-1]; race[l-1] = hold;
+				}
+			}
+		}
+									
+
+if (sim[g] == 99 && sim[g-1] !=champ)
+		{for (int l =0; l<race.size(); l++){ if (race[l] == sim[g-1]){race[l] = 0;}
+    }
+}
+
+race.erase(remove(race.begin(), race.end(), 0), race.end());
+}
+
+
+
+cout << "Race Results:";
+for (size_t i =0; i<race.size(); i++)
+{cout <<race[i] << " ";}
+
+
+cout << "\n";
+for (size_t i =0; i<race.size(); i++)
+{
+if (race[i] == champ){ cout << i+1;}
+}
+
 
 }
