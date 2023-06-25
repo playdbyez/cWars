80e6049516f1633da3d3ab37f76e27d35571d659
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-06-25 10:36:44 +0100

    Update CommonT00L.cpp

--- CommonT00L.cpp
+++ CommonT00L.cpp
@@ -1,25 +1,39 @@
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//Timer
+#include<chrono>
+auto start = std::chrono::high_resolution_clock::now();
+//Code here
+auto stop = std::chrono::high_resolution_clock::now();
+auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 
+cout << "\n" << duration.count() << " microseconds" ;
 
-
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 //If element exists in vector
 if (std:: find (v.begin(), v.end(), elem) != v.end ())
-{/*elem exists in the vector*/}
+{
+//elem exists in the vector
+}
 
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 //Determine lenght of a single int with a multi-digit value
 int len = to_string(n).length();
 
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 //Remove specific element
 vec.erase(remove(vec.begin(), vec.end(), 0), vec.end());
 
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 //Even or Odd
 return (num %2 ==0)?"Even":"Odd";
 
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 //find position of minElement
 auto it = std::min_element(std::begin(vec), std::end(vec));
 std::cout << "index of smallest element: " << std::distance(std::begin(vec), it);
 
-
-
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 //Do/While
 do{ } 
         while(next_permutation(v.begin(),v.end()));
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
