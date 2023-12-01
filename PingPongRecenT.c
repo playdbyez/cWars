213913f1e979fddac36b51175401257a390e7408
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-12-01 16:27:49 +0000

    Update PingPongRecenT.c

--- PingPongRecenT.c
+++ PingPongRecenT.c
@@ -1,3 +1,148 @@
+
+#include <string.h>
+#include <stdio.h>
+
+int main()
+{
+    
+    //pong-ping-pong-ping-pong-ping-pong-ping-pang-pong-ping-pong-ping-pong-ping-pong-ping-pong-ping-slap-klak-pong-ping-pong-ping-pong-ping-pong-funk-ding-plop-ping-pong-ping-pong-ping-pong-ping-pong-ping-pong-clak-ding
+    //Pong
+    
+    
+    //ping-pong-ding-pong-ping-donk-ping-thud
+    //Pong
+    
+    //pong-ping-bink-pong-ping-donk-pong-ping-pong-bangping-pong-bink-ping-pong-donk-pong-ping-pong-ping-bangping-pong-aaaa-ping-pong-ping-aaaapong-ping-pong-ping-aaaa-pong-ping-pong-aaaa
+    //Pong
+ 
+ 
+ 
+ char *sounds = "pong-ping-bink-pong-ping-donk-pong-ping-pong-bangping-pong-bink-ping-pong-donk-pong-ping-pong-ping-bangping-pong-aaaa-ping-pong-ping-aaaapong-ping-pong-ping-aaaa-pong-ping-pong-aaaa";
+ unsigned bin =0; 
+  unsigned bon =0;
+
+  unsigned game = strlen(sounds);
+
+char pink[5] = {'p','i','n','g'};
+char ponk[5] = {'p','o','n','g'};
+  int offs[10];
+  int nxt = 0;
+  
+  char dip[20];
+  char song[20];
+  
+  int q = 0;
+  int b = 0;
+  int d = 0;
+  int z = 0;
+  
+  
+  
+  for (int i = 0; i<game; i++){
+    if(sounds[i-1] == '-' ){ 
+        
+      if (sounds [i-5] == 'p' && sounds[i-4] =='i' ){ 
+          
+          for (int x =0; x < strlen(ponk)-1; x++){
+                    if (sounds[i+x] != ponk[x]){
+                                                offs[nxt] = i-1;
+                                                nxt++;
+                                                offs[nxt +1] = '\0';
+                                                printf("%c ", sounds[i-1]);break;   }
+        }
+      }
+      if (sounds [i-5] == 'p' && sounds[i-4] =='o' ){
+          for (int y =0; y < strlen(pink)-1; y++){
+          if (sounds[i+y] != pink[y]){
+                                                offs[nxt] = i-1;
+                                                nxt++;
+                                                offs[nxt +1] = '\0';
+                                                printf("%c ", sounds[i-1]);break;   }
+        }   
+      }
+      
+    }
+  }
+ printf("\n\n");
+  int l =0;
+ int t =0;
+ 
+ 
+ while (offs[l] != '\0'){
+     t = offs[l];
+     for (int k = t; k<game;k++){
+         
+         if (sounds[offs[l]-3] == 'i' && sounds[offs[l]-4] == 'p'){dip[b] = 'i'; dip[b+1] = '\0'; b++; l++;k = t;}
+         
+        if (sounds[offs[l]-3] == 'o' && sounds[offs[l]-4] == 'p'){dip[b] = 'o'; dip[b+1] = '\0'; b++; l++;k = t;}
+     }
+     l++;
+ }
+
+ 
+ 
+     if (sounds[1] == 'o' ){song[q] = 'o';  q++; }
+     if (sounds[1] == 'i' ){song[q] = 'i';  q++; }
+ 
+ 
+ 
+ for (int j = 0; j<game; j++){
+     
+     if (j  == offs[d]){while(j < offs[d+1]){
+        if (sounds[j] == 'p' && sounds[j+1] == 'i')
+            {song[q] = 'i'; song[q+1]= '\0'; q++;d++;break;}
+         if (sounds[j] == 'p' && sounds[j+1] == 'o')
+            {song[q] = 'o'; song[q+1]= '\0'; q++;d++;break;}
+                                                    j++;    }
+     }
+ }
+ 
+ unsigned drops = strlen(dip);
+  unsigned serve = strlen(song);
+  
+ 
+printf("Serves: %s\n", song);
+printf("Drops : %s",dip);
+
+for (int n = 0; n<serve; n++){
+                            if(song[n] == 'i' && dip[n] == 'o') {bin++;}
+                            if(song[n] == 'o' && dip[n] == 'i') {bon++;}
+}
+
+if (bon == bin){
+   for (size_t j = game; j>0; j--){
+     if (sounds[j] == 'g'){if (sounds[j-1] == 'n'){if (sounds[j-2] == 'o'){if (sounds[j-3] == 'p'){ bin++;break;}}}}
+     if (sounds[j] == 'g'){if (sounds[j-1] == 'n'){if (sounds[j-2] == 'i'){if (sounds[j-3] == 'p'){ bon++;break;}}}}
+   }  
+ }
+ 
+ if (bin>bon){printf("\nping");}
+ if (bon>bin){printf ("\npong");}
+  
+   return 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include <string.h>
 #include <stdio.h>
 
