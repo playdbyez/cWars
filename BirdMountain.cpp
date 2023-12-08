f228282e4ed99c557394391169eba52de5296051
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-12-08 13:13:59 +0000

    Update BirdMountain.cpp

--- BirdMountain.cpp
+++ BirdMountain.cpp
@@ -168,3 +168,121 @@ int main()
     cout  << "\nHighest peak: ";
     
 }
+
+
+//ALMOS7 D0NE BIII DAF0K!?
+
+
+
+
+// Example program
+#include <iostream>
+#include <string>
+#include<vector>
+using namespace std;
+
+
+int main()
+{
+    unsigned sum = 0;
+  vector<string> mountain = {
+            "^^^^^^        ",
+            " ^^^^^^^^     ",
+            "  ^^^^^^^     ",
+            "  ^^^^^       ",
+            "  ^^^^^^^^^^^ ",
+            "  ^^^^^^      ",
+            "  ^^^^        "
+        };
+       
+    
+    int inc =1;
+    char level = '1';
+       
+        for (size_t i = 0; i < mountain.size(); ++i)
+        {
+            for (size_t j = 0; j < mountain[i].size(); ++j){
+           
+        if ( i == 0 && mountain[i][j] == '^'){mountain[i][j] = '1';}
+        if ( i == mountain.size()-1 && mountain[i][j] == '^'){mountain[i][j] = level;}
+       
+        if ( mountain[i][j] == '^' && isspace(mountain[i+1][j])){mountain[i][j] = level;}
+        if ( mountain[i][j] == '^' && isspace(mountain[i-1][j])){mountain[i][j] = level;}
+        if ( mountain[i][j] == '^' && isspace(mountain[i][j-1])){mountain[i][j] = level;}
+        if ( mountain[i][j] == '^' && isspace(mountain[i][j+1])){mountain[i][j] = level;}
+            }
+        }
+        
+        
+           
+    
+    bool dn0 = 0;
+    bool dn1 = 0;
+    
+    bool dd1 = 0;
+    bool dd2 = 0;
+    
+    
+    int tob = -1;
+    int bot = 0;
+    
+    //while(tob != bot){ 
+   for (size_t g = 0; g < mountain.size(); ++g){ if (dn0 == 1){break;}
+                for (size_t q = 0; q < mountain[g].size(); ++q){
+                    if (mountain[g][q] == '^' ){tob = g;
+                                                dn0=1;
+                                                cout << "top = " << tob;
+                                                break;}                     }
+                    
+                
+                if (g == mountain.size()-1 && dn0 == 0) {tob =0;break;}
+            }
+            
+            
+            
+                        for (size_t g = mountain.size()-1;g>0; --g){if (dn1 == 1){break;}
+                for (size_t q = mountain[g].size();q>0; --q){
+                    if (mountain[g][q] == '^' ){bot = g;
+                                                dn1 =1;
+                                                cout << "bottom = " << bot<<endl;
+                                                break;}                             } 
+                    
+                if (g == 1 && dn1 == 0) {bot =0;break;}
+            }
+    
+    //if(tob == bot){break; cout << "okay!?"; }
+    if (tob != bot){level++;}
+        for(size_t v = 0; v< mountain[tob].size();v++){if (mountain[tob][v] == '^' ){mountain[tob][v] = level;}}
+        for(size_t w = 0; w< mountain[bot].size();w++){if (mountain[bot][w] == '^' ){mountain[bot][w] = level;}}
+        
+    
+    
+          for (size_t o = 0; o < mountain.size(); ++o ){
+                for (size_t l = 0; l < mountain[o].size(); ++l)
+                {if (mountain[o][l] == '^' ){ 
+                                            mountain [o][l] = level;
+                                            break;  }   }
+                for (size_t g = mountain[o].size();g > 0; --g)
+                        {if (mountain[o][g] == '^' ){ 
+                                                    mountain [o][g] = level;
+                                                    break;}} 
+                                                        
+                    
+                }
+            
+
+          
+            
+    //cout << "loop count: ";
+        //cout << "top= " << tob << " bottom= " << bot <<"\n\n";
+    //if(tob == bot){cout << "WE GOOD \n";break;  }
+        
+                                                                                                             
+    
+    for (size_t v = 0; v < mountain.size(); ++v ){
+            for (size_t u = 0; u < mountain[v].size(); ++u){ cout << mountain[v][u];}cout << "\n";}
+          
+
+    cout  << "\nHighest peak: ";
+    
+}
