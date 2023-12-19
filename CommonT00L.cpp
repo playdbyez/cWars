87899b88456a4bc6709b02035464cb1b48865219
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-12-19 12:34:47 +0000

    Update CommonT00L.cpp

--- CommonT00L.cpp
+++ CommonT00L.cpp
@@ -15,6 +15,12 @@ if (std:: find (v.begin(), v.end(), elem) != v.end ())
 //elem exists in the vector
 }
 
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//cpp11 multidimensional array + vector
+int a1[3][3] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+std::vector<std::vector<int>> a2 = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 //Determine lenght of a single int with a multi-digit value
 int len = to_string(n).length();
