f7c38b8382ab7e4e2180867c4335bbcb92ee6db8
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-12-19 12:14:57 +0000

    Update CountingDuplicates.cpp

--- CountingDuplicates.cpp
+++ CountingDuplicates.cpp
@@ -1,9 +1,11 @@
+#include <iostream>
 #include <vector>
 #include <string>
 #include <cstring>
 #include <algorithm>
 using namespace std;
 
+int main () {
 string in = "zaddupl1cat1onSSZ";
  string inn;
 vector <int> l1;
@@ -11,9 +13,9 @@ string l2;
 vector <int> reps;
 int inc =1;
 int em = 0;
+vector <char> who;
 
-  
-  for (size_t c =0; c<strlen(in); c++) {inn+=in[c];}
+  for (size_t c =0; c<in.size(); c++) {inn+=in[c];}
   
   if (inn.empty()){em =0;}
 
@@ -30,10 +32,13 @@ for (size_t a = 0; a<inn.size(); a++){
 sort(l2.begin(), l2.end());
 for (size_t d = 0; d < l2.size(); d++){ 
 	if (l2[d] == l2[d+1]){inc++;}
-	if (l2[d] != l2[d+1] || d == l2.size()-1){ if (inc > 1)reps.push_back(inc); inc=1;}
+	if (l2[d] != l2[d+1] || d == l2.size()-1){ if (inc > 1){reps.push_back(inc); inc=1; who.push_back(l2[d]);}}
 	
 }
 
 em = reps.size();
-  cout << em;
+  cout << "The total ammount of reocurring characters is : "<< em << endl;
+  
+  for (size_t x = 0; x < who.size(); x ++){cout << "Character ["<< who[x]<< "] repeats " << reps[x] << " times" << endl;}
+  
 }
