1004b8964003b74bdd668afcb47d388f02ab5146
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-12-22 16:44:03 +0000

    Update Snail.cpp

--- 4Kyu/Snail.cpp
+++ 4Kyu/Snail.cpp
@@ -156,6 +156,91 @@ vector<std::vector<int>> snail_map = {{1,2,3,4}, {12,13,14,5}, {11,16,15,6}, {10
 
 
 
+
+
+  ////
+
+  //Expected: equal to [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 ]
+
+//Actual: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 13, 14 ]
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    
+
+vector<std::vector<int>> snail_map = {{1,2,3,4}, {12,13,14,5}, {11,16,15,6}, {10,9,8,7}};
+  vector <int> sero = {} ;
+  vector <int> sn = {1} ;
+  vector <int> spath = {} ;
+  
+  //Zero
+  //if (snail_map[0].size() == 0){ return sero;}
+  //cout << snail_map.size()<<endl;
+
+  //One
+  //if (snail_map[0].size() == 1){ return sn;}
+   
+  int scol = snail_map.size();
+  int srow = -1;
+   if (scol > 1){srow = snail_map[0].size();}
+  
+  
+  int dr = scol-1;
+  int ul = -1;
+  int ur = srow -1;
+  int dl = 0;
+  
+  int carj = 0;
+  int cari = 0;
+  int totalsize = 0;
+  
+  for (size_t i = 0; i < scol; i++){for (size_t j = 0; j < srow; j++){ totalsize++;}}
+ 
+  
+  
+
+  
+  if (scol > 1){
+    for (size_t i =0; i < scol; i++)
+    { ul++; i = ul; 
+      for (size_t j =0; j < srow; j++)
+      { 
+          spath.push_back(snail_map[i][j]);
+           if ( spath.size() == totalsize){ cout << "Size match!" <<endl;break;}
+          
+          
+          srow = snail_map[i].size();
+          ur = srow -1;
+          
+           if ( i == ul){ j = carj;
+                }
+        
+            if (j == ur){  ur --;carj = j;
+                            while(i < dr ){ i++; spath.push_back(snail_map[i][j]); }  
+                                if ( spath.size() == totalsize){ cout << "Size match!!" <<endl;} }
+  
+  
+  
+        if (i == dr) {  dr--; j = carj; cari = i;
+                        while(j > dl  ){j--;spath.push_back(snail_map[i][j]);} 
+                            if ( spath.size() == totalsize){ cout << "Size match!!!" <<endl;}  }
+                        
+            if (j == dl) { dl++;  i= cari; 
+                        while (i > ul ){ i--;spath.push_back(snail_map[i][j]);} if ( spath.size() == totalsize){ cout << "Size match" <<endl;}} 
+               
+                              
+      }
+    }
+  }
+  
+    for (size_t m =0; m < spath.size(); m++){
+            cout << spath[m] << " ";
+    }
+}
   
 
 
