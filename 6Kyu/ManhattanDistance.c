d9cdd2cd94adf90dfbf7538bfe30b9019b25995c
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-01-16 12:20:33 +0000

    Update ManhattanDistance.c

--- 6Kyu/ManhattanDistance.c
+++ 6Kyu/ManhattanDistance.c
@@ -66,6 +66,105 @@ for (int t = 0; t < verx; t++ )
 
 printf("\n %d", anz);
  return 0;
+
+
+
+
+    //alted
+
+
+
+
+
+
+
+    
+// Expected 4
+#include <stdio.h>
+
+int main()
+{
+    int a[2] = {80, 20}; int b[2] = {46, 90};
+    unsigned inc = 0;
+    unsigned anz = 1;
+    char turns = '0';
+   
+  int totala = a[0] > b[0] ?  a[0]: b[0];  
+  int botala = a[1] > b[1] ?  a[1]: b[1];
+  
+  if (totala  == 1) {totala = 2;}
+char board[botala+1][totala+1];
+
+unsigned horiz= sizeof board[0];
+unsigned verx= sizeof board / horiz;
+
+//Player Start Position and add the minus to the destination
+int playsx = a[0]+1;
+if (playsx > 0) {playsx-=1;}
+//if (playsx == 0) {playsx-=1;}
+int playsy = a[1]+1;
+if (playsy > 0) {playsy-=1;}
+//Destination Position
+int destx = b[0]+1;;
+if (destx > 0) {destx-=1;}
+int desty = b[1]+1;;
+if (desty > 0) {desty-=1;}
+
+
+// Populate Map
+for (int t = 0; t < horiz; t++ )
+{for (int p = 0; p < verx; p++ )
+ {
+  board[t][p] = 'O';
+  if (t == playsx && p  == playsy){board[t][p] = 'X';}
+  if (t == destx && p  == desty ){board[t][p] = 'Z';} 
+ }
+}
+
+while (inc < sizeof board)
+{
+    if (playsx > destx && playsy == desty){ playsx--;
+				        turns ++; anz++;
+					board[playsx][playsy] = turns;
+    if (playsx == destx && playsy == desty){ break;}}
+  
+
+
+    if (playsx < destx && playsy == desty){ playsx++;
+				        turns ++; anz++;
+					board[playsx][playsy] = turns;
+    if (playsx == destx && playsy == desty){ break;}}
+
+
+ if (playsxy > desty && playsx == destx){ playsy--;
+				        turns ++; anz++;
+					board[playsx][playsy] = turns;
+    if (playsx == destx && playsy == desty){ break;}}
+  
+
+
+    if (playsy < desty && playsx == destx){ playsy++;
+				        turns ++; anz++;
+					board[playsx][playsy] = turns;
+    if (playsx == destx && playsy == desty){ break;}}
+
+  
+  
+
+inc++;
+}
+
+//Display Map
+for (int t = 0; t < verx; t++ )
+{for (int p = 0; p < horiz; p++ )
+ {printf("%c", board[t][p]); }printf("\n");
+}
+
+printf("\n %d", anz);
+ return 0;
+ 
+}
+
  
 }
 
