4e7cd71998e290f1058a7bdc5617f13e8b70eebe
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-01-23 12:09:44 +0000

    Update Passwordgenerator.cpp

--- 6Kyu/Passwordgenerator.cpp
+++ 6Kyu/Passwordgenerator.cpp
@@ -6,47 +6,43 @@ using namespace std;
 int main () {
 
 random_device rd;
-uniform_int_distribution<int> dist(1,3);
+string pass;
 uniform_int_distribution<int> ups(65 , 90);
 uniform_int_distribution<int> low(97 , 122);
-uniform_int_distribution<int> nub(48 , 57);
+uniform_int_distribution<int> nu(48 , 57);
+
+
+  
+  //Random distribution of char and n#
 uniform_int_distribution<int> siz(6 , 20);
-  string pass;
-  
-  bool lc = false;
-  bool uc = false;
-  bool nu = false;
-  
-  int min = 6;
-  int max = 20;
-  int range = max - min;
- 
-  int pox = 0;
-  
-size_t num = siz(rd);
-  
-  for (size_t i = 0; i < num; i++){
+uniform_int_distribution<int> dist(1,3);
+int pox = 0;
+size_t len = siz(rd);
+  for (size_t i = 0; i < len; i++){
     pox = dist(rd);
    
     if (pox == 1){ pass+= ups(rd);} 
     if (pox == 2){ pass+= low(rd);}
-    if (pox == 3){ pass+= nub(rd);} 
+    if (pox == 3){ pass+= nu(rd);} 
+}
+//Allocating requirement positions
+  uniform_int_distribution<int> st(0 , len-1);
+  int req [3];
+  bool al = 0;
+  while (al == false){
+    req[0] = rand () % len -  6 + 6;
+    req[1] = st(rd);
+    req[2] = rand () % len -  6 + 6;
+      if (req[0] != req[1] && req[0] != req[2] && req[1] != req[2]){al = true;}  }
+   int n = sizeof(req) / sizeof(req[0]);
+  sort (req, req + n);
                                 
-  }
+ //Ensuring requirments
+  // Needs randomization in between (0, 1, 2) provided by next permutation
+  pass[req[2]] = ups(rd);
+  pass[req[1]] = nu(rd);
+  pass[req[0]] = low(rd);
   
-  
-            for (int j = 0; j < pass.size(); j++)
-            {
-             if (islower(pass[j])) {lc = true;}
-             if (isupper(pass[j])) {uc = true;}
-             if (isdigit(pass[j])) {nu = true;}
-              
-                            if (j == pass.size()-1){ if (lc == false){cout << "LL" <<endl;pass[rand() %6 +1 ] = rand() % (122 - 97) + 97;}
-                                                     if (uc == false){cout << "Uc" <<endl;pass[rand() %9 +1] = rand() % (90 - 65) + 65;}
-                                                     if (nu == false){cout << "NU" <<endl;pass[rand() %8 +1 ] = rand() % (57 - 48) +48;}
-                                                 }
-
-            }
   cout << pass << endl;
 
   
