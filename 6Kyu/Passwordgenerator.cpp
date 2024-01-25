796f9f87bb1121665fb3120a43568acb03ee115a
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-01-25 13:19:31 +0000

    Update Passwordgenerator.cpp

--- 6Kyu/Passwordgenerator.cpp
+++ 6Kyu/Passwordgenerator.cpp
@@ -20,25 +20,30 @@ int pox = 0;
 size_t len = siz(rd);
   for (size_t i = 0; i < len; i++){
     pox = dist(rd);
-   
-    if (pox == 1){ pass+= ups(rd);} 
-    if (pox == 2){ pass+= low(rd);}
-    if (pox == 3){ pass+= nu(rd);} 
+    //if (pox %2 != 0 && pass.size() <len - 2){ pass+= low(rd);}
+    if (pox == 3){ pass+= nu(rd);}if (pox == 1){ pass+= ups(rd);}
+    if (pox == 2){ pass+= low(rd);}  
+    
 }
+  
 //Allocating requirement positions
   uniform_int_distribution<int> st(0 , len-1);
+  uniform_int_distribution<int> sig(0 , len-1);
   int req [3];
   bool al = 0;
   while (al == false){
-    req[0] = rand () % len -  6 + 6;
+    pox = sig(rd);
+    int nuly = st(rd);
+    req[0] = nuly;
     req[1] = st(rd);
-    req[2] = rand () % len -  6 + 6;
+    req[2] = sig(rd);
       if (req[0] != req[1] && req[0] != req[2] && req[1] != req[2]){al = true;}  }
+  if (pox %2 == 0 && len < 19){pass[req[1]]+=ups(rd);}
+  if (pox %2 != 0 && len < 19){pass[req[2]]+=low(rd);}
    int n = sizeof(req) / sizeof(req[0]);
   sort (req, req + n);
                                 
  //Ensuring requirments
-  // Needs randomization in between (0, 1, 2) provided by next permutation
   pass[req[2]] = ups(rd);
   pass[req[1]] = nu(rd);
   pass[req[0]] = low(rd);
