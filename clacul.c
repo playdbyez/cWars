3260d87790ec6e42e32c8e4f1f05ef18e32b5456
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-02-04 12:46:05 +0000

    Update clacul.c

--- clacul.c
+++ clacul.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
@@ -10,13 +11,29 @@ int y = 29;
 size_t xlen = floor(log10(x))+1;
 size_t ylen = floor(log10(y))+1;
 
+int carry =0;
+int res=0;
+
+
+unsigned n = 0; // malloc incrementr
+unsigned m = 0; // exponent incrementr
+
+char *final;
+
 
 char buffx[xlen];
 char buffy[ylen];
 
 sprintf(buffx ,"%d",x);
 sprintf(buffy ,"%d",y);
-printf("%s\n%s",buffy,buffx);
+
+int bas;     // last char of multiplicand X
+int expo;    // last char of multipliyer Y
+expo =buffy[strlen(buffy)-1] -48;
+bas =buffx[strlen(buffx)-1] -48;
+
+//printf("%s\n%s",buffy,buffx);
+printf("%d",expo *bas);
     
     
     
