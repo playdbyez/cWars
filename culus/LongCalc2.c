b28afbdd39571be7ed0cf626a16f46b968450af6
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-02-26 17:18:29 +0000

    Update LongCalc2.c

--- culus/LongCalc2.c
+++ culus/LongCalc2.c
@@ -2,28 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
  
  
 int main()
 {
-/*
-unsigned x = 1000;
-unsigned y = 1012;
- 
-int xlen = log10(x)+1;
-int ylen = log10(y)+1;
- 
- 
-char buffx[xlen+1];
-char buffy[ylen+1];
- 
-sprintf(buffx ,"%d",x);
-sprintf(buffy ,"%d",y);
- 
- */
+clock_t start , end;
 char *buffx = "34570005056892984637486820";
 char *buffy = "40820245540029171906865204";
- 
 // Target result = "1411156094742421775373509194368931047469571066611280";
  
  
@@ -44,19 +30,17 @@ unsigned long long n = 1;
 unsigned long long m = 1;
 unsigned long long t = 1; 
 unsigned long long o = 0;
-unsigned long long q = 1;
+unsigned long long q = 0;
  
  
-char *final;
-    final = malloc(q*xlen+1);
+char final[xlen*xlen];
  
-for (unsigned long long i = strlen(buffy) ; i > 0 ; i--){  
- 
-expo = buffy[strlen(buffy)-m] -48;
-carry = 0;
- 
+
+for (unsigned long long i = strlen(buffy) ; i > 0 ; i--){       expo = buffy[strlen(buffy)-m] -48;
+ carry = 0;
     for (unsigned long long j = strlen(buffx) ; j > 0 ; j--){
-        bas  = buffx[strlen(buffx)-n] -48;
+ 
+                                                bas  = buffx[strlen(buffx)-n] -48;
         sum  = expo * bas;
         //printf("expo is = %d\n", expo);
         //printf("bas is = %d\n", bas);
@@ -66,49 +50,39 @@ carry = 0;
         //printf("sum is = %d\n", sum);
         sprintf(buffsum,"%lld", sum);
         if (zrz == 1 ){carry = 0;}
-                            if(j != 1){
-                                        if(strlen(buffsum) > 1)
-                                              { buffs1[0] = buffsum[0];
-                                                buffs2[0] = buffsum[1];
+        if(j != 1){
+            if(strlen(buffsum) > 1){
+                    buffs1[0] = buffsum[0];
+                    buffs2[0] = buffsum[1];
+                    if (strlen(buffsum) > 1)carry     = buffs1[0]-48;
+                    final[o]  = buffs2[0];
+                    o++;
+            }
  
-                                               if (strlen(buffsum) > 1)
-                                               {carry = buffs1[0]-48;}
+            if (strlen(buffsum) == 1){
  
-                                               final[o]  = buffs2[0];
-                                                o++;q++;
-                                                final = realloc(final, q*xlen); }
+                    if (strlen(buffsum) > 1){carry  = buffs1[0]-48;}
+                    if (strlen(buffsum) == 1){carry  = 0;}
+                    final[o]  = buffsum[0];
+                    o++;
  
-                                        if (strlen(buffsum) == 1)
-                                              { if (strlen(buffsum) > 1)
-                                              {carry  = buffs1[0]-48;}
- 
-                                                if (strlen(buffsum) == 1)
-                                                {carry  = 0;}
- 
-                                                final[o]  = buffsum[0];
-                                                o++;q++;
-                                                final = realloc(final, q*xlen); }
-                                                                                        }
+            }
+        }
  
         if (j == 1 ){
-            if (strlen(buffsum) > 1)
-                      {
-                        final[o] = buffsum[1];o++;q++;
-                        final[o] = buffsum[0];o++;q++;
-                        final = realloc(final, q*xlen); 
-                                                             }
-                            if (strlen(buffsum) == 1)
-                            {final[o] = buffsum[0];o++;}
-                                                                      }            
+            if (strlen(buffsum) > 1){
+        final[o] = buffsum[1];o++;
+        final[o] = buffsum[0];o++;
+            }
+            if (strlen(buffsum) == 1){final[o] = buffsum[0];o++;}
+        }            
         n++;
     }
-if (i != 1)final[o] = '-';
-carry = 0;
-o++;
-q++;
-n =1;
-m++;
-final = realloc(final, q*xlen);
+    if (i != 1)final[o] = '-';
+    carry = 0;
+    o++;
+    n =1;
+    m++;
 }
  
  
@@ -148,51 +122,32 @@ maxy++;
 maxx+=incd; 
 
 char *tabler[maxx][maxy];
-
-
-o = 0;
-
-for (int i = 0; i < maxy; i ++)
-{for (int j = maxx; j > 0; j --)
-{
-    if (j < spcline[o])
-    
-}
-
-   
  
-  
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-
-    //Output Tabler
- 
-  
- 
-    printf("\n\n");
+printf("\n\n");
     for (unsigned i = 0; i < maxy; i++){
      for (unsigned j = 0; j < maxx; j++){
         printf("%d ", tabler[i][j]);
  
      }
         printf("\n");
-    }
+    } 
  
+
  
-     //char tabler[maxx][maxy];
-    //printf("\n%d  & %d & %d", maxx, maxy, incd);
- 
-//printf("\n\n%s", final);
- 
+    end= clock();
+
+// 1.0 seconds
+// 0.1 = tenth of a second
+// 0.01 = hundreth of a second
+// 0.001 = thousandth of a second = milisecond
+// 0.000001 = milionth of a second = microsecond
+printf("\n%f ms", (double) (end-start)  / CLOCKS_PER_SEC);
  
 return 0;
 } 
  
+  
+ 
+    
+ 
+
