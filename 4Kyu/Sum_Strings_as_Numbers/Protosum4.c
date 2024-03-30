3adc5e9c40f4ab976fb78dbc1d5f2e85d5e5f0ed
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-03-30 15:42:33 +0000

    Create Protosum4.c

---
+++ 4Kyu/Sum_Strings_as_Numbers/Protosum4.c
@@ -0,0 +1,55 @@
+//https://rextester.com/l/c_online_compiler_clang
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+
+int main ()
+{
+    char *a = "712569312664357328695151392";
+    char *b = "8100824045303269669937";
+   int len1 = strlen(a);
+   int len2 = strlen(b);  
+   int max  = len1>len2 ? len1: len2;
+   unsigned long internz;
+  
+  char sumtable[2][max];
+  
+          int long tabley = sizeof (sumtable)/ sizeof sumtable [0];
+          int long tablex = sizeof sumtable [0]/ sizeof sumtable [0][0];
+  int long long iter = tablex-1;
+  
+     memset(sumtable, '0', sizeof(sumtable));
+  
+for (int long long i = len1-1; i > -1; i--)
+    {if (iter>-1){sumtable[0][iter] = a[i];iter--;}}
+ iter = tablex -1;
+  for (int long long i = len2-1; i > -1; i--)
+    {if (iter>-1){sumtable[1][iter] = b[i];iter--;}}
+  
+    
+  
+    for (int i = tablex; i > 0; i--){    
+        for (int j = 1; j < tabley; j++){
+        
+            printf("%c", sumtable[j][i]);
+        
+        
+        }
+        printf ("\n");
+    }
+  
+          
+  
+ 
+  
+  // OUTPUT sumtable
+    for (int i = 0; i < tabley; i++){
+      for (int j = 0; j < tablex; j++)
+      { 
+        printf("%c", sumtable[i][j]);
+      }
+  printf ("\n");    
+  }
+  
+    //printf ("%d"tablezx);
+}
