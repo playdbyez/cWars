d25b4110582412895f8fe9cddcb3af7121ef71f4
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-06-01 11:06:03 +0100

    Update and rename PyCopyDotNyenn.c to almyst.c

--- culus/almyst.c
+++ culus/almyst.c
@@ -22,9 +22,8 @@ Node *Listin(Node *head, char val)
 
 char *Listerate (Node *head, char *arr, int len)
 {
-  printf("\nPrinting the whole list:\n");
-        int bil = 0;
-        int uti = 0;
+        unsigned bil = 0;
+        unsigned uti = 0;
         Node *pl = head;
         while (pl != NULL )
                 {
@@ -48,9 +47,9 @@ void Listend(Node *head)
  
 int main()
 {
-clock_t start , end;
-char *buffx = "34570005056892984637486820";
-char *buffy = "40820245540029171906865204";
+
+char *buffx = "34570005056892984637486820";       // Buffx's strlen() cannot be longer  than Buffy
+char *buffy = "40820245540029171906865204569";
 // Target result = "1411156094742421775373509194368931047469571066611280";
  
 
@@ -72,13 +71,13 @@ char * buffs2;
 int zrz = 0;
 unsigned gig;
 gig = xlen > ylen ? xlen : ylen;
- 
+printf("%d\n", gig); 
  
 unsigned long long sum    = 0;
 unsigned long long carry  = 0;
 unsigned long long n = 1; 
 unsigned long long m = 1;
-unsigned long long t = 1; 
+unsigned long long t = 1; //huh?
 unsigned long long o = 0;
 unsigned long long q = 0;
 unsigned long long w = 0;
@@ -151,7 +150,7 @@ for (unsigned long long i = strlen(buffy) ; i > 0 ; i--){
 int flen = q;
  
  
-free (buffsum);
+
 free (buffs2);
 o = 0;
 sum   = 0;
@@ -180,16 +179,19 @@ while (o < gig){ *(spcline+o) = o; o++;}
     free (final);
 
     //Continue..
-    
+    memset (buffsum, '\0', sizeof(char));
+    buffsum = malloc(sizeof(char)*3);
     Node * nd = NULL;
     unsigned bl = 0;
     
-    for (int i = tablex-1; i > -1; i--){    
-        for (int j = 0; j < tabley; j++){     sum += sumtable[j][i] - 48;   }
+    for (int i = szt2-1; i > -1; i--){    
+        for (int j = 0; j < szt1; j++){     sum += tablz[j][i] - 48;   }
         sum+= carry;
                 sprintf(buffsum, "%d", sum);
+          
         if (strlen(buffsum) == 1){ nd = Listin(nd, buffsum[0]); bl++; carry = 0;}
         if (strlen(buffsum) == 2){ nd = Listin(nd, buffsum[1]); bl++; carry =buffsum[0]-48; }
+        if (strlen(buffsum) == 3){ buffsum = realloc (buffsum, sizeof(char)*4); nd = Listin(nd, buffsum[2]); bl++; carry =sum/10; buffsum[2] = '\0'; }
         
         if (i == 0 && strlen(buffsum) == 2) {nd = Listin(nd, buffsum[1]); bl++;nd = Listin(nd, buffsum[0]); bl++; carry = 0;}
         if (i == 0 && strlen(buffsum) == 1) { carry = 0;}
@@ -197,9 +199,12 @@ while (o < gig){ *(spcline+o) = o; o++;}
         sum = 0;
     }
     
+    free (buffsum);
     char *buff;
   buff = malloc(sizeof (char) * (bl+1));
-    
+  buff = Listerate(nd, buff,bl+1);  
+    printf("%s", buff);
+    //for (int i = 0; i < bl+1; i++){printf("%c",buff[i]); }
     
     /* 
     //Output tablz
