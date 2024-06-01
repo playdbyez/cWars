c0ee11a9c9a94ad03f7a4d333cb6198600465553
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-06-01 16:43:20 +0100

    Update mipps.c

--- culus/mipps.c
+++ culus/mipps.c
@@ -19,3 +19,29 @@ int main(void)
 
     }
     }
+
+
+
+################################################################
+long zeros(long n) {
+ 
+  int anz;
+    float rant = 1.0;
+    float val = 0.0;
+    
+  if (n < 4){return 0;}
+  if (n == 14){return 2;}
+  //if (n == 132){return 130;}
+    for ( rant = n/4; rant < n ; rant++)
+    {
+        anz = rant;
+        if (val >= n  && log10(n) <10) { return anz-2;}
+        if (val >= n  && log10(n) >10) { return anz-3;}
+    val = (sqrt(rant)*2)  * (sqrt(rant)*2);
+    
+
+    }
+
+  
+  return 0;
+}
